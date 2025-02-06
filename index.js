bb9af3ac4201e0c1c383bf0654e605b1241c4642
Martin-ing <23002494@galileo.edu>
2025-02-06 11:35:09 -0600

    agregando multiples ciudades

--- index.js
+++ index.js
@@ -7,35 +7,41 @@ form.addEventListener("submit", async (event) => {
   const city = document.getElementById("cityInput").value.trim();
   resultDiv.innerHTML = "";
 
-  if (!city) {
-    resultDiv.innerHTML =
-      '<div class="alert alert-danger">Ingrese el nombre de una ciudad!</div>';
-    return;
-  }
-
   try {
     const apiKey = "d49f768bd5ea4f249972455f214cadfe";
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-    );
+    const cities = city.split(",").map(city => city.trim());
 
-    if (!response.ok) {
-      throw new Error("La ciudad no se encontro");
+    if (cities.length === 0) {
+      resultDiv.innerHTML = '<div class="alert alert-danger">Invalid input!</div>';
+      return;
     }
 
-    const data = await response.json();
-
-    resultDiv.innerHTML = `
-      <div class="card result-card p-3">
-        <div class="card-body text-center">
-          <h5 class="card-title">${data.name}, ${data.sys.country} <i class="fas fa-map-marker-alt text-info"></i></h5>
-          <p class="mb-2"><i class="fas fa-temperature-high text-danger"></i> <strong>${data.main.temp}°C</strong></p>
-          <p class="mb-2"><i class="fas fa-cloud text-primary"></i> ${data.weather[0].description}</p>
-          <p class="mb-0"><i class="fas fa-tint text-info"></i> Humedad: ${data.main.humidity}%</p>
-        </div>
-      </div>
-    `;
-  } catch (error) {
-    resultDiv.innerHTML = `<div class="alert alert-danger">${error.message}</div>`;
+    for (const city of cities) {
+  
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+        );
+  
+        if (!response.ok) {
+          throw new Error(`City "${city}" not found`);
+        }
+  
+        const data = await response.json();
+  
+        resultDiv.innerHTML += `
+          <div class="card result-card p-3 mb-3">
+            <div class="card-body text-center">
+              <h5 class="card-title">${data.name}, ${data.sys.country} <i class="fas fa-map-marker-alt text-info"></i></h5>
+              <p class="mb-2"><i class="fas fa-temperature-high text-danger"></i> <strong>${data.main.temp}°C</strong></p>
+              <p class="mb-2"><i class="fas fa-cloud text-primary"></i> ${data.weather[0].description}</p>
+              <p class="mb-0"><i class="fas fa-tint text-info"></i> Humidity: ${data.main.humidity}%</p>
+            </div>
+          </div>
+        `;
+    }
   }
-});
+  catch (error) {
+    resultDiv.innerHTML += `<div class="alert alert-danger">${error.message}</div>`;
+  }
+}
+);
